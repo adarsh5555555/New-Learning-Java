2217661d581f9b99b7861b32c6274b2f1d13ff30
Adarsh <adarsh80810@gmail.com>
2025-06-08 00:19:59 +0530

    add it to my repo

---
+++ TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student myStu = new Student("Adarsh",21);
+        Student myStu2 = new Student("darshit",22);
+        System.out.println( myStu.getName());
+        System.out.println(myStu.getAge());
+        System.out.println(myStu.getAge()); myStu2.getAge();
+        myStu2.getName();
+    }
+}
