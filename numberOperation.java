eac8602b14a2409a28f2b0709079525085a6488e
Adarsh <adarsh80810@gmail.com>
2025-06-10 02:42:57 +0530

    add it to my repo

--- numberOperation.java
+++ numberOperation.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class numberOperation {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.print("Enter the number in which you want to find :");
+        //       System.out.print("Enter the number in which you want to find :");
         /*
+
         this was by simple algorithem and operation on numbers
 
          */
@@ -22,19 +23,70 @@ public class numberOperation {
 //        }
 //        System.out.printf("number of occurence of %d is %d",num2,i);
         /*
+
         now doing the same problem by String operation
+
          */
-        String num  = inp.next();
-        System.out.println("Enter the digit whose number of occurence you have to find :");
-        int num2 = inp.nextInt();
-        int i=0;
-        while(i<num.length()){
-            char c = num.trim().charAt(i);
-            if(c==num2){
-                i++;
-            }
-            i++;
-        }
-        System.out.printf("the number of occurence of %d is %d",num,i);
+//        String num  = inp.next();
+//        System.out.println("Enter the digit whose number of occurence you have to find :");
+//        String num2 = inp.next();
+//        char numm = num2.trim().charAt(0);
+//        System.out.println(numm);
+//        int j=0;
+//        int i=0;
+//        while(i<num.length()){
+//            char c = num.trim().charAt(i);
+//            if(c==numm){
+//                j++;
+//            }
+//            i++;
+//   }
+        //logic to reverse a number
+//        System.out.print("ENTER THE NUMBER YOU WANT TO REVERSE :");
+//        int number = inp.nextInt();
+//        int reversedDigit = 0;
+//        while(number>0){
+//            reversedDigit  = reversedDigit*10+number%10;
+//            number /= 10;
+//        }
+//        System.out.printf("your reversed digit is %d",reversedDigit);
+        
+        
+      //      creating calculator
+
+
+//
+//        System.out.println("Enter the two numbers");
+//        int num1 = inp.nextInt();
+//        int num2 = inp.nextInt();
+//
+//        while (true) {
+//            System.out.println("Enter the operator you want to operate with :");
+//            char operator = inp.next().trim().charAt(0);
+//            if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+//                if (operator == '+') {
+//                    System.out.println(num1 + num2);
+//                } else if (operator == '-') {
+//                    System.out.println(num1 - num2);
+//                } else if (operator == '*') {
+//                    System.out.println(num1 * num2);
+//                } else if (operator == '/') {
+//                    if (num2 != 0) {
+//                        System.out.println(num1 / num2);
+//                    } else {
+//                        System.out.println("the divesion is invalied");
+//                    }
+//                }
+//
+//            } else if (operator == 'x' || operator == 'X') {
+//                break;
+//            }
+//        }
+
+
     }
 }
+
+
+
+
