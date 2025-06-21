16474301b1e86c4a11b2799bf521fbd4393aaf3d
Adarsh <adarsh80810@gmail.com>
2025-06-21 17:32:04 +0530

    update my repo

---
+++ SeaarchingInInfiniteArray.java
@@ -0,0 +1,16 @@
+public class SeaarchingInInfiniteArray {
+    public static void main(String[] args) {
+
+    }
+    static int find(int []arr , int target){
+        int start = 0;
+        int end =1;
+        while (target>arr[end]) {
+
+            start = end;
+
+        }
+
+    }
+    static int search(int []arr , int target ,)
+}
